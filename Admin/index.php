03bc3f92c00b95b966684e29c3de240a473b2bcb
elmehdiben20 <mehdibenhmidi51@gmail.com>
2022-09-15 09:54:20 +0100

    commit

--- Admin/index.php
+++ Admin/index.php
@@ -15,178 +15,142 @@ if (!isset($_SESSION['Email1'])) {
 <!doctype html>
 <html lang="en">
 
-    
+
 <!-- Mirrored from themesbrand.com/minible/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 31 May 2022 13:14:34 GMT -->
 <?php include('include/header.php'); ?>
 
-    
-    <body>
+
+<body>
 
     <!-- <body data-layout="horizontal" data-topbar="colored"> -->
 
-        <!-- Begin page -->
-        <div id="layout-wrapper">
+    <!-- Begin page -->
+    <div id="layout-wrapper">
         <?php include('include/sidebar.php'); ?>
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="main-content">
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
 
-                <div class="page-content">
-                    <div class="container-fluid">
+            <div class="page-content">
+                <div class="container-fluid">
 
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box d-flex align-items-center justify-content-between">
-                                    <h4 class="mb-0">Dashboard</h4>
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="mb-0">Dashboard</h4>
 
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Minible</a></li>
-                                            <li class="breadcrumb-item active">Dashboard</li>
-                                        </ol>
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Minible</a></li>
+                                        <li class="breadcrumb-item active">Dashboard</li>
+                                    </ol>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+                    <?php
+                    include('include/db.php');
+                    $query1 = "SELECT * , sum(qty*price) as somme  from medicines , medicines_order where medicines.id=medicines_order.id_m and delivery='Delivery Done'";
+                    $rst1 = $con->query($query1);
+                    $row1 = mysqli_fetch_array($rst1);
+                    ?>
+                    <div class="row">
+                        <div class="col-md-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="float-end mt-2">
+                                        <div id="total-revenue-chart"></div>
+                                    </div>
+                                    <div>
+                                        <h4 class="mb-1 mt-1"><span data-plugin="counterup"><?php echo $row1['somme'] ?></span> DHS</h4>
+                                        <p class="text-muted mb-0">Total Revenue</p>
                                     </div>
 
                                 </div>
                             </div>
-                        </div>
-                        <!-- end page title -->
-
-                        <div class="row">
-                            <div class="col-md-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="float-end mt-2">
-                                            <div id="total-revenue-chart"></div>
-                                        </div>
-                                        <div>
-                                            <h4 class="mb-1 mt-1">$<span data-plugin="counterup">34,152</span></h4>
-                                            <p class="text-muted mb-0">Total Revenue</p>
-                                        </div>
-                                       
+                        </div> <!-- end col-->
+                        <?php
+                        $query2 = "SELECT * , count(id) as idd  from  medicines_order where is_deleted='0'";
+                        $rst2 = $con->query($query2);
+                        $row2 = mysqli_fetch_array($rst2);
+                        ?>
+                        <div class="col-md-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="float-end mt-2">
+                                        <div id="orders-chart"> </div>
                                     </div>
-                                </div>
-                            </div> <!-- end col-->
-
-                            <div class="col-md-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="float-end mt-2">
-                                            <div id="orders-chart"> </div>
-                                        </div>
-                                        <div>
-                                            <h4 class="mb-1 mt-1"><span data-plugin="counterup">5,643</span></h4>
-                                            <p class="text-muted mb-0">Orders</p>
-                                        </div>
-                                       
+                                    <div>
+                                        <h4 class="mb-1 mt-1"><span data-plugin="counterup"><?php echo $row2['idd'] ?></span></h4>
+                                        <p class="text-muted mb-0">Orders</p>
                                     </div>
-                                </div>
-                            </div> <!-- end col-->
 
-                            <div class="col-md-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="float-end mt-2">
-                                            <div id="customers-chart"> </div>
-                                        </div>
-                                        <div>
-                                            <h4 class="mb-1 mt-1"><span data-plugin="counterup">45,254</span></h4>
-                                            <p class="text-muted mb-0">Customers</p>
-                                        </div>
-                                       
+                                </div>
+                            </div>
+                        </div> <!-- end col-->
+                        <?php
+                        $query3 = "SELECT count(id) as user  from  users ";
+                        $rst3 = $con->query($query3);
+                        $row3 = mysqli_fetch_array($rst3);
+                        ?>
+                        <div class="col-md-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="float-end mt-2">
+                                        <div id="customers-chart"> </div>
                                     </div>
-                                </div>
-                            </div> <!-- end col-->
-
-                            <div class="col-md-6 col-xl-3">
-
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="float-end mt-2">
-                                            <div id="growth-chart"></div>
-                                        </div>
-                                        <div>
-                                            <h4 class="mb-1 mt-1">+ <span data-plugin="counterup">12.58</span>%</h4>
-                                            <p class="text-muted mb-0">Growth</p>
-                                        </div>
-                                      
+                                    <div>
+                                        <h4 class="mb-1 mt-1"><span data-plugin="counterup"><?php echo $row3['user'] ?></span></h4>
+                                        <p class="text-muted mb-0">Customers</p>
                                     </div>
+
                                 </div>
-                            </div> <!-- end col-->
-                        </div> <!-- end row-->
+                            </div>
+                        </div> <!-- end col-->
 
-                        <div class="row">
-                            <div class="col-xl-8">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h4 class="card-title mb-4">Sales Analytics</h4>
+                        <div class="col-md-6 col-xl-3">
 
-                                        <div class="mt-1">
-                                            <ul class="list-inline main-chart mb-0">
-                                                <li class="list-inline-item chart-border-left me-0 border-0">
-                                                    <h3 class="text-primary">$<span data-plugin="counterup">2,371</span><span class="text-muted d-inline-block font-size-15 ms-3">Income</span></h3>
-                                                </li>
-                                                <li class="list-inline-item chart-border-left me-0">
-                                                    <h3><span data-plugin="counterup">258</span><span class="text-muted d-inline-block font-size-15 ms-3">Sales</span>
-                                                    </h3>
-                                                </li>
-                                                <li class="list-inline-item chart-border-left me-0">
-                                                    <h3><span data-plugin="counterup">3.6</span>%<span class="text-muted d-inline-block font-size-15 ms-3">Conversation Ratio</span></h3>
-                                                </li>
-                                            </ul>
-                                        </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="float-end mt-2">
+                                        <div id="growth-chart"></div>
+                                    </div>
+                                    <div>
+                                        <h4 class="mb-1 mt-1">+ <span data-plugin="counterup">12.58</span>%</h4>
+                                        <p class="text-muted mb-0">Growth</p>
+                                    </div>
 
-                                        <div class="mt-3">
-                                            <div id="sales-analytics-chart" class="apex-charts" dir="ltr"></div>
-                                        </div>
-                                    </div> <!-- end card-body-->
-                                </div> <!-- end card-->
-                            </div> <!-- end col-->
-
-                            <div class="col-xl-4">
-                                <div class="card bg-primary">
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="col-sm-8">
-                                                <p class="text-white font-size-18">Enhance your <b>Campaign</b> for better outreach <i class="mdi mdi-arrow-right"></i></p>
-                                                <div class="mt-4">
-                                                    <a href="javascript: void(0);" class="btn btn-success waves-effect waves-light">Upgrade Account!</a>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <div class="mt-4 mt-sm-0">
-                                                    <img src="assets/images/setup-analytics-amico.svg" class="img-fluid" alt="">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div> <!-- end card-body-->
-                                </div> <!-- end card-->
-
-                            </div> <!-- end Col -->
-                        </div> <!-- end row-->
-
-               
+                                </div>
+                            </div>
+                        </div> <!-- end col-->
+                    </div> <!-- end row-->
 
 
 
 
-                    </div> <!-- container-fluid -->
-                </div>
-                <!-- End Page-content -->
-     <?php include('include/footer.php'); ?>
+
+
+                </div> <!-- container-fluid -->
             </div>
-            <!-- end main content-->
-
+            <!-- End Page-content -->
+            <?php include('include/footer.php'); ?>
         </div>
-        <!-- END layout-wrapper -->
+        <!-- end main content-->
 
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
+    </div>
+    <!-- END layout-wrapper -->
 
-       <?php include('include/js.php'); ?>
-    </body>
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
+
+    <?php include('include/js.php'); ?>
+</body>
 
 
 <!-- Mirrored from themesbrand.com/minible/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 31 May 2022 13:15:28 GMT -->
+
 </html>
